8c0ef30542dca590b8d999cd7b4bb50af3b05c28
Abhilash Asokan <abhilash.asokan@logicline.de>
2022-01-12 21:34:43 +0530

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Abhilash on 12/01/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
